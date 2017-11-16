0badd087fa499c705d315a1aec0eeee37c4070e3
jiangshuai <jiangshuai@hujiang.com>
2017-11-16 16:15:15 +0800

    爬虫练习

--- crawler.py
+++ crawler.py
@@ -1,15 +1,42 @@
 from lxml import html
 import requests
+import pprint
+
+import pymysql.cursors
+conn = pymysql.connect(host='localhost',
+                       user='root',
+                       password='root',
+                       db='blog',
+                       charset='utf8mb4',
+                       cursorclass=pymysql.cursors.DictCursor)
 
 def search(url):
   page = requests.get(url)
   tree = html.fromstring(page.text)
 
-  names = tree.xpath('//div[@class="hd"]/a/span[@class="title"][1]/text()')
+  mvEles = tree.xpath('//div[@class="item"]')
 
-  print('nams', names)
-  for x in names:
-    print(x)
+  movies = [];
+
+  try:
+    for ele in mvEles:
+      movie = {}
+      img = ele.xpath('./div[@class="pic"]//@src')
+      link = ele.xpath('./div[@class="info"]//a/@href')
+      names = ele.xpath('./div[@class="info"]//a//span/text()')
+      desc = ele.xpath('.//p/text()')
+      keywords = ele.xpath('.//span[@class="inq"]/text()')
+      with conn.cursor() as cursor:
+        # Create a new record
+        sql = "INSERT INTO `movies` (`link`, `name`, `desc`,`keywords`, `img`) VALUES (%s, %s, %s, %s, %s)"
+        cursor.execute(sql, (''.join(link) ,''.join(names), ''.join(desc).strip(), ''.join(keywords), ''.join(img)))
+      conn.commit()
+  finally:
+    conn.close();
+
+  # print('nams', names)
+  # for x in names:
+  #   print(x)
 
 if __name__ == '__main__':
   search('https://movie.douban.com/top250')
